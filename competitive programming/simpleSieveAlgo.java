a4191a73b6961c4e5c74d68e5d754e85e8159e8c
Jitendra-Sharma <jitendra.sharma7375@gmail.com>
2024-01-23 15:22:40 +0530

    modify

--- competitive programming/simpleSieveAlgo.java
+++ competitive programming/simpleSieveAlgo.java
@@ -3,25 +3,29 @@ import java.util.Scanner;
 
 public class simpleSieveAlgo {
     public static void main(String[] args) {
+        // higher 10^6, 10^8
         int n;
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number: ");
         n = sc.nextInt();
-        boolean prime[] = new boolean[n+1];
+        boolean prime[] = new boolean[n + 1];
         Arrays.fill(prime, true);
         prime[0] = false;
         prime[1] = false;
-        for(int i = 2; i*i <= n; i++) {
-            if(prime[i] == true) {
-                for(int j = i*i; j <= n; j += i) {
-                    prime[j] = false;
+        for(int i=0;i*i<=n;i++)
+        {
+            if(prime[i]==true)
+            {
+                for(int j=i*i;j<=n;j+=i)
+                {
+                    prime[j]=false;
                 }
             }
         }
-        for(int i = 0; i <= n; i++) {
-            if(prime[i] == true) {
-                System.out.print(i + " ");
-            }
+        for(int i=0;i<n;i++)
+        {
+            if(prime[i]==true)
+                System.out.print(i+" ");
         }
         sc.close();
     }
